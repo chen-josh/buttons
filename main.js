27d5b0a7d2ac57dc68dda489bee3255d1be45d5d
josh <73039734+chen-josh@users.noreply.github.com>
2022-02-07 15:51:30 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("hello this is josh it's feb. 7")
+
+const pic_buttons = document.getElementsByName("pic-button")
+const text = document.querySelector("#text")
+const photo = document.querySelector("#photo")
+
+function change_pic(pic) {
+    text.innerHTML = "here's a picture of " + pic + "."
+    photo.src = "../images/" + pic + ".jpg" 
+}
+
+pic_buttons.forEach(button => {
+    button.addEventListener("change", event => {
+        change_pic(button.value)
+    })
+})
